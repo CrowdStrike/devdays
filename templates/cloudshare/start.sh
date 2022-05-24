7845882b943c8b4444f5144b7da085729e536ab9
Justin Harris <justin.harris@crowdstrike.com>
2022-05-25 00:17:35 +0100

    Added templates to S3_Bucket folder

--- templates/cloudshare/start.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-UNIQUE=$(cat /tmp/environment.txt | cut -c -8 | tr _ - | tr '[:upper:]' '[:lower:]')
-cd /home/ec2-user
-/home/ec2-user/build/cwp-se-demo_v2.sh up --unique_id="$UNIQUE" --trusted="$(curl -s http://ipinfo.io/ip)/32"
