29ef7f0ddece4b0bc471a1e5836434ec7da41d10
Rob Solomon <solorob33@gmail.com>
2022-12-22 11:26:18 -0500

    lab 8 image assessment changes

--- tools/create_code-zip.sh
+++ tools/create_code-zip.sh
@@ -3,5 +3,5 @@
 # run from the tools folder before commiting changes made to files in the ./code folder to update code.zip in the templates folder.
 
 cd ../code
-zip -r code.zip *
+zip -r code.zip * -x "*/\.DS_Store"
 mv ./code.zip ../templates
